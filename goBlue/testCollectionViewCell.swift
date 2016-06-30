f83d0119aaa9a266e6f111feaf660ba7e7f3d903
kawaxman <kent.waxman18@thebenjaminschool.org>
2016-06-29 21:06:26 -0400

    Added testCollectionViewCell.

---
+++ goBlue/testCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  testCollectionViewCell.swift
+//  goBlue
+//
+//  Created by School on 6/29/16.
+//  Copyright © 2016 Kent Waxman. All rights reserved.
+//
+
+import UIKit
+
+class testCollectionViewCell: UICollectionViewCell {
+    
+}
